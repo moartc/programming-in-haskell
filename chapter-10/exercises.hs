1f5f122a9930d8dda9227e57433c84fa8ee0214c
szwester <allenszwester@gmail.com>
2022-01-15 14:30:54 +0100

    Reformat with brittany and apply hlint suggestions

--- chapter-10/exercises.hs
+++ chapter-10/exercises.hs
@@ -1,121 +1,128 @@
-import System.IO
-import Data.Char
-import Distribution.Simple.Program (getDbProgramOutput)
-import Distribution.Fields.LexerMonad (getInput)
+import           Data.Char
+import           Distribution.Fields.LexerMonad ( getInput )
+import           Distribution.Simple.Program    ( getDbProgramOutput )
+import           System.IO
 
 
 -- Exercise 1
 putStr1 :: String -> IO ()
-putStr1 [] = return ()
-putStr1 (x:xs) = do
-                 putChar x
-                 putStr1 xs
+putStr1 []       = return ()
+putStr1 (x : xs) = do
+   putChar x
+   putStr1 xs
 
-putStr' xs = sequence_ [putChar x | x <- xs]
+putStr' xs = sequence_ [ putChar x | x <- xs ]
 
 -- Exercise 2
 type Board = [Int]
 initial :: Board
-initial = [5,4,3,2,1]
+initial = [5, 4, 3, 2, 1]
 
 
 putRow :: Int -> Int -> IO ()
-putRow row num = do putStr (show row)
-                    putStr ": "
-                    putStrLn (concat (replicate num "* "))
+putRow row num = do
+   putStr (show row)
+   putStr ": "
+   putStrLn (concat (replicate num "* "))
 
 putBoard :: Board -> IO ()
-putBoard [a,b,c,d,e] = do putRow 1 a
-                          putRow 2 b
-                          putRow 3 c
-                          putRow 4 d
-                          putRow 5 e
+putBoard [a, b, c, d, e] = do
+   putRow 1 a
+   putRow 2 b
+   putRow 3 c
+   putRow 4 d
+   putRow 5 e
 
 putBoard' :: Board -> IO ()
 putBoard' b = putBoardAux b 1
 
 putBoardAux :: Board -> Int -> IO ()
-putBoardAux [] _ = return ()
-putBoardAux (b:bs) r = do putRow r b
-                          putBoardAux bs (r+1)
+putBoardAux []       _ = return ()
+putBoardAux (b : bs) r = do
+   putRow r b
+   putBoardAux bs (r + 1)
 
 
 -- Exercise 3
 putBoard'' :: Board -> IO ()
-putBoard'' bs = sequence_ [putRow r b | (r,b) <- zip [1..] bs]
+putBoard'' bs = sequence_ [ putRow r b | (r, b) <- zip [1 ..] bs ]
 
 -- Exercise 4
-newline :: IO()
+newline :: IO ()
 newline = putChar '\n'
 
 getDigit :: IO Int
-getDigit = do x <- getChar
-              if isDigit x then
-                 return (digitToInt x)
-              else
-                 do putStrLn "ERROR: Invalid digit"
-                    getDigit
+getDigit = do
+   x <- getChar
+   if isDigit x
+      then return (digitToInt x)
+      else do
+         putStrLn "ERROR: Invalid digit"
+         getDigit
 
 
 readNumbers :: Int -> IO [Int]
 readNumbers 0 = return []
-readNumbers x = do n <- getDigit
-                   newline
-                   ns <- readNumbers (x-1)
-                   return (n:ns)
+readNumbers x = do
+   n <- getDigit
+   newline
+   ns <- readNumbers (x - 1)
+   return (n : ns)
 
 sumDigits :: [Int] -> Int
-sumDigits [] = 0
-sumDigits (x:xs) = x + sumDigits xs
+sumDigits = sum
 
 
 adder :: IO ()
-adder = do putStr "How many numbers? "
-           nb <- getDigit
-           newline
-           nums <- readNumbers nb
-           putStr "The total is "
-           putStrLn (show (sum nums))
+adder = do
+   putStr "How many numbers? "
+   nb <- getDigit
+   newline
+   nums <- readNumbers nb
+   putStr "The total is "
+   print (sum nums)
 
 -- Exercise 5
 getDigit' :: IO Int
-getDigit' = do x <- getChar
-               newline
-               if isDigit x then
-                  return (digitToInt x)
-               else
-                  do putStrLn "ERROR: Invalid digit"
-                     getDigit'
+getDigit' = do
+   x <- getChar
+   newline
+   if isDigit x
+      then return (digitToInt x)
+      else do
+         putStrLn "ERROR: Invalid digit"
+         getDigit'
 getDigits 0 = []
-getDigits n = getDigit' : getDigits (n-1)
+getDigits n = getDigit' : getDigits (n - 1)
 
 adder' :: IO ()
-adder' = do putStr "How many numbers? "
-            nb <- getDigit'
-            nums <- sequence (getDigits nb)
-            putStr "The total is "
-            print (sum nums)
+adder' = do
+   putStr "How many numbers? "
+   nb   <- getDigit'
+   nums <- sequence (getDigits nb)
+   putStr "The total is "
+   print (sum nums)
 
 -- Exercise 6
 getCh :: IO Char
-getCh = do hSetEcho stdin False
-           x <- getChar
-           hSetEcho stdin True
-           return x
+getCh = do
+   hSetEcho stdin False
+   x <- getChar
+   hSetEcho stdin True
+   return x
 
 readLine = readLine' ""
-readLine' xs  = do x <- getCh
-                   if x == '\n' then
-                      do putChar x
-                         return xs
-                   else
-                      if x == '\DEL' then
-                         do putStr "\b \b"                            
-                            if not (null xs) then
-                               readLine' (init xs)
-                            else
-                               readLine' ""
-                      else
-                         do putChar '-'
-                            readLine' (xs ++ [x])
-                            
+readLine' xs = do
+   x <- getCh
+   if x == '\n'
+      then do
+         putChar x
+         return xs
+      else if x == '\DEL'
+         then do
+            putStr "\b \b"
+            if not (null xs) then readLine' (init xs) else readLine' ""
+         else do
+            putChar '-'
+            readLine' (xs ++ [x])
+
--- chapter-10/life.hs
+++ chapter-10/life.hs
@@ -4,11 +4,12 @@ cls = putStr "\ESC[2j]"
 type Pos = (Int, Int)
 
 writeat :: Pos -> String -> IO ()
-writeat p xs = do goto p
-                  putStr xs
+writeat p xs = do
+    goto p
+    putStr xs
 
 goto :: Pos -> IO ()
-goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
 
 width :: Int
 width = 10
@@ -19,51 +20,61 @@ height = 10
 type Board = [Pos]
 
 glider :: Board
-glider = [(4,2), (2,3), (4,3), (3,4), (4,4)]
+glider = [(4, 2), (2, 3), (4, 3), (3, 4), (4, 4)]
 
 showcells :: Board -> IO ()
-showcells b = sequence_ [writeat p "0" | p <- b]
+showcells b = sequence_ [ writeat p "0" | p <- b ]
 
 isAlive :: Board -> Pos -> Bool
-isAlive b p = elem p b
+isAlive b p = p `elem` b
 
 isEmpty :: Board -> Pos -> Bool
 isEmpty b p = not (isAlive b p)
 
 neighbs :: Pos -> [Pos]
-neighbs (x,y) = map wrap [(x-1, y-1), (x, y-1),
-                          (x+1, y-1), (x-1, y),
-                          (x+1, y), (x-1, y+1),
-                          (x, y+1), (x+1, y+1)]
+neighbs (x, y) = map
+    wrap
+    [ (x - 1, y - 1)
+    , (x    , y - 1)
+    , (x + 1, y - 1)
+    , (x - 1, y)
+    , (x + 1, y)
+    , (x - 1, y + 1)
+    , (x    , y + 1)
+    , (x + 1, y + 1)
+    ]
 
 wrap :: Pos -> Pos
-wrap (x,y) = (((x-1) `mod` width) + 1,
-              ((y-1) `mod` height) + 1)
+wrap (x, y) = ((x - 1) `mod` width + 1, (y - 1) `mod` height + 1)
 
 liveneighbs :: Board -> Pos -> Int
 liveneighbs b = length . filter (isAlive b) . neighbs
 
 survivors :: Board -> [Pos]
-survivors b = [p | p <-b, elem (liveneighbs b p) [2,3]]
+survivors b = [ p | p <- b, liveneighbs b p `elem` [2, 3] ]
 
 births :: Board -> [Pos]
-births b = [(x,y) | x <- [1..width],
-                    y <- [1..height],
-                    isEmpty b (x,y),
-                    liveneighbs b (x,y) == 3]
+births b =
+    [ (x, y)
+    | x <- [1 .. width]
+    , y <- [1 .. height]
+    , isEmpty b (x, y)
+    , liveneighbs b (x, y) == 3
+    ]
 
 rmdups :: Eq a => [a] -> [a]
-rmdups [] = []
-rmdups (x:xs) = x : rmdups (filter (/= x) xs)
+rmdups []       = []
+rmdups (x : xs) = x : rmdups (filter (/= x) xs)
 
 nextgen :: Board -> Board
 nextgen b = survivors b ++ births b
 
 life :: Board -> IO ()
-life b = do cls
-            showcells b
-            wait 500000
-            life (nextgen b)
+life b = do
+    cls
+    showcells b
+    wait 500000
+    life (nextgen b)
 
 wait :: Int -> IO ()
-wait n = sequence_ [return () | _ <- [1..n]]
+wait n = sequence_ [ return () | _ <- [1 .. n] ]
