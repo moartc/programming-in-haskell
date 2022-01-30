0167964b63fdb769250ccab0e2a4ac14d319ae23
szwester <allenszwester@gmail.com>
2022-01-30 23:37:24 +0100

    Solutions to the next exercises.

--- chapter-11/exercise4c.hs
+++ chapter-11/exercise4c.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE FlexibleContexts #-}
 import           Data.Char
 import           Data.List
-import           DynamicLoading (getValueSafely)
 import           System.IO
 
---size :: Int
---size = 0
+size :: Int
+size = 3
 
 type Grid = [[Player]]
 data Player = O | B | X
@@ -16,11 +14,11 @@ next O = X
 next B = B
 next X = O
 
-empty :: Int -> Grid
-empty s = replicate s (replicate s B)
+empty :: Grid
+empty = replicate size (replicate size B)
 
 full :: Grid -> Bool
-full = notElem B . concat
+full = all (/=B) . concat
 
 turn :: Grid -> Player
 turn g = if os <= xs then O else X
@@ -38,15 +36,15 @@ wins p g = any line (rows ++ cols ++ dias)
         dias = [diag g, diag (map reverse g)]
 
 diag :: Grid -> [Player]
-diag g = [g !! n !! n | n <- [0..length g -1]]
+diag g = [g !! n !! n | n <- [0..size-1]]
 
 won :: Grid -> Bool
 won g = wins O g || wins X g
 
 putGrid :: Grid -> IO ()
-putGrid g =
-    putStrLn . unlines . concat . interleave bar . map showRow $ g
-    where bar = [replicate ((length g *4)-1) '-']
+putGrid =
+    putStrLn . unlines . concat . interleave bar . map showRow
+    where bar = [replicate ((size*4)-1) '-']
 
 showRow :: [Player] -> [String]
 showRow = beside . interleave bar . map showPlayer
@@ -65,11 +63,11 @@ interleave x [y]    = [y]
 interleave x (y:ys) = y : x : interleave x ys
 
 valid :: Grid -> Int -> Bool
-valid g i = 0 <= i && i < length g^2 && concat g !! i == B
+valid g i = 0 <= i && i < size^2 && concat g !! i == B
 
 move :: Grid -> Int -> Player -> [Grid]
 move g i p =
-    [chop (length g) (xs ++ [p] ++ ys) | valid g i]
+    if valid g i then [chop size (xs ++ [p] ++ ys)] else []
     where (xs,B:ys) = splitAt i (concat g)
 
 chop :: Int -> [a] -> [[a]]
@@ -77,7 +75,7 @@ chop n [] = []
 chop n xs = take n xs : chop n (drop n xs)
 
 getNat :: String -> IO Int
-getNat prompt = do putStrLn prompt
+getNat prompt = do putStr prompt
                    xs <- getLine
                    if xs /= [] && all isDigit xs then
                        return (read xs)
@@ -85,28 +83,8 @@ getNat prompt = do putStrLn prompt
                        do putStrLn "ERROR: Invalid number"
                           getNat prompt
 
-tictactoe :: IO ()
-tictactoe = run (empty 3) O
-
-run :: Grid -> Player -> IO ()
-run g p = do cls
-             goto (1,1)
-             putGrid g
-             run' g p
-
-run' :: Grid -> Player -> IO ()
-run' g p | wins O g = putStrLn "Player O wins!\n"
-         | wins X g = putStrLn "Player X wins!\n"
-         | full g   = putStrLn "It's a draw!\n"
-         | otherwise =
-             do i <- getNat (prompt p)
-                case move g i p of
-                    [] -> do putStrLn "ERROR: Invalid move"
-                             run' g p
-                    [g'] -> run g' (next p)
-
 cls :: IO ()
-cls = putStr "\ESC[2j]"
+cls = putStr "\ESC[2J"
 
 type Pos = (Int, Int)
 
@@ -114,7 +92,7 @@ goto :: Pos -> IO ()
 goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
 
 prompt :: Player -> String
-prompt p = "Player " ++ show p ++ ", enter your move: "
+prompt p = "Player " ++ show p ++ ", enter your move"
 
 data Tree a = Node a [Tree a]
               deriving Show
@@ -126,7 +104,7 @@ moves :: Grid -> Player -> [Grid]
 moves g p
     | won g     = []
     | full g    = []
-    | otherwise = concat [move g i p | i <- [0..((length g^2)-1)]]
+    | otherwise = concat [move g i p | i <- [0..((size^2)-1)]]
 
 prune :: Int -> Tree a -> Tree a
 prune 0 (Node x _)  = Node x []
@@ -147,37 +125,59 @@ minimax (Node g ts)
                         ts' = map minimax ts
                         ps  = [p | Node(_,p) _ <- ts']
 
-bestmove :: Grid -> Player -> Grid
-bestmove g p = head [g' | Node (g',p') _ <- ts, p' == best]
+bestmove :: Grid -> Player -> Tree Grid -> Grid
+bestmove g p gt = head [g' | Node (g',p') _ <- ts, p' == best]
                where
-                   tree = prune depth (gametree g p)
+                   tree = prune depth gt
                    Node (_,best) ts = minimax tree
 
 main :: IO ()
 main = do hSetBuffering stdout NoBuffering
-          size <- getNat "Set size"
-          putStrLn "Press 'O' if you want to play first or 'X' if second!\n"
-          c <- getChar
-          if c == 'O' then
-              play (empty size) O
-          else
-              play (empty size) X
-
+          play empty O
 
 play :: Grid -> Player -> IO ()
 play g p = do cls
-              goto (1,30)
+              goto (1,1)
               putGrid g
-              play' g p
+              play' g p $ gametree g p
 
-play' :: Grid -> Player -> IO ()
-play' g p | wins O g = putStrLn "Player O wins!\n"
-          | wins X g = putStrLn "Player X wins!\n"
-          | full g   = putStrLn "It's a draw!\n"
-          | p == O = do i <- getNat (prompt p)
-                        case move g i p of
-                             [] -> do putStrLn "ERROR: Invalid move"
-                                      play' g p
-                             [g'] -> play g' (next p)
-          | p == X = do putStr "Player X is thinking... "
-                        (play $! (bestmove g p)) (next p)
+play' :: Grid -> Player -> Tree Grid -> IO ()
+play' g p gt | wins O g = putStrLn "Player O wins!\n"
+             | wins X g = putStrLn "Player X wins!\n"
+             | full g   = putStrLn "It's a draw!\n"
+             | p == O = do i <- getNat (prompt p)
+                           case move g i p of 
+                                 [] -> do putStrLn "ERROR: Invalid move"
+                                          play' g p gt
+                                 [g'] -> play g' (next p)
+             | p == X = do putStr "Player X is thinking... "
+                           (play $! (bestmove g p gt)) (next p)
+
+
+
+-- tests for part c
+testGrid :: Grid
+testGrid = [[X,B,O],
+            [X,B,B],
+            [B,B,O]]
+
+gridToFind :: Grid
+gridToFind = [[X,X,O],
+              [X,B,B],
+              [B,B,O]]
+
+gamet :: Tree Grid
+gamet = gametree testGrid X
+
+findGrid :: Eq a => a -> Tree a -> Tree a
+findGrid toFind xs = head [a | a <- getY xs, getX a == toFind]
+
+getX :: Tree a -> a
+getX (Node x y) = x
+
+getY :: Tree a -> [Tree a]
+getY (Node x y) = y
+
+found = findGrid gridToFind gamet
+
+
