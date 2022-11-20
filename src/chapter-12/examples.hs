18c358e5f335234a00cca5a55fba6ce508039ce1
szwester <allenszwester@gmail.com>
2022-11-20 22:31:56 +0100

    Examples from chapter 12

--- src/chapter-12/examples.hs
+++ src/chapter-12/examples.hs
@@ -1,11 +1,18 @@
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving (Show)
+
+instance Functor Tree where
+  fmap g (Leaf x) = Leaf (g x)
+  fmap g (Node l r) = Node (fmap g l) (fmap g r)
+
+ex1 :: Tree Int
+ex1 = fmap length (Leaf "abc")
+
+ex2 :: Tree Bool
+ex2 = fmap even (Node (Leaf 1) (Leaf 2))
+
 inc :: Functor f => f Int -> f Int
 inc = fmap (+ 1)
 
-prods :: [Int] -> [Int] -> [Int]
-prods xs ys = [x * y | x <- xs, y <- ys]
-
-p1 = prods [1, 2] [2, 3]
-
-prods' :: [Int] -> [Int] -> [Int]
-prods' xs ys = pure (*) <*> xs <*> ys
+ex3 = inc [1, 2, 3, 4, 5] 
+ex4 = inc (Just 1)
 
