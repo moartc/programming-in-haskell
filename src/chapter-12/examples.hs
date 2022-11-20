66574621b637f19093c1c5443197260579d67547
szwester <allenszwester@gmail.com>
2022-11-20 18:48:52 +0100

    Migrating to stack project

--- src/chapter-12/examples.hs
+++ src/chapter-12/examples.hs
@@ -1,10 +1,11 @@
 inc :: Functor f => f Int -> f Int
-inc = fmap (+1)
+inc = fmap (+ 1)
 
 prods :: [Int] -> [Int] -> [Int]
-prods xs ys = [x*y | x <- xs, y <- ys]
-p1 = prods [1,2] [2,3]
+prods xs ys = [x * y | x <- xs, y <- ys]
+
+p1 = prods [1, 2] [2, 3]
 
 prods' :: [Int] -> [Int] -> [Int]
 prods' xs ys = pure (*) <*> xs <*> ys
-p2 = prods' [1,2] [2,3]
+
