940cb5887ddb4c2e2706d79cdb9e8943c6333945
szwester <allenszwester@gmail.com>
2023-01-17 23:06:57 +0100

    Foldables part 1

--- src/chapter-14/examples.hs
+++ src/chapter-14/examples.hs
@@ -1,9 +1,43 @@
-import Data.Monoid
+import Data.Monoid (All (All), Any (Any), Product (Product, getProduct), Sum (..))
+import Data.Foldable (Foldable(fold), Foldable(foldr))
 
-t1 = mconcat [Sum 2, Sum 3, Sum 4]
+-- Monoids
 
-t2 = mconcat [Product 2, Product 3, Product 4]
+t1 = mconcat [Sum 2, Sum 3, Sum 4] -- Sum {getSum = 9}
 
-t3 = mconcat [All True, All True, All True]
+t2 = mconcat [Product 2, Product 3, Product 4] -- Product {getProduct = 24}
 
-t4 = mconcat [Any False, Any False, Any False]
+t3 = mconcat [All True, All True, All True] -- All {getAll = True}
+
+t4 = mconcat [Any False, Any False, Any False] -- Any {getAny = False}
+
+-- Foldables
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+fold :: Monoid a => Tree a -> a
+fold (Leaf x) = x
+fold (Node l r) = Data.Foldable.fold l `mappend` Data.Foldable.fold r
+
+t5 = getSum (foldMap Sum [1 .. 10]) -- 55
+
+t6 = getProduct (foldMap Product [1 .. 10]) -- 3628800
+
+instance Foldable Tree where
+    fold (Leaf x) = x
+    fold (Node l r) = Data.Foldable.fold l `mappend` Data.Foldable.fold r
+
+    foldMap f (Leaf x) = f x
+    foldMap f (Node l r) = foldMap f l `mappend` foldMap f r
+
+    foldr f v (Leaf x ) = f x v
+    foldr f v (Node l r ) = foldr f (foldr f v r) l
+
+    foldl f v (Leaf x ) = f v x
+    foldl f v (Node l r) = foldl f (foldl f v l) r
+
+tree :: Tree Int
+tree = Node (Node (Leaf 1) (Leaf 2)) (Leaf 3)
+
+t7 = foldr (+) 0 tree -- 6
+t8 = foldl (+) 0 tree -- 6
